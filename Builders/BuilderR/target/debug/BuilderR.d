0ff43bc6d9fec3441a49447fb82d3d7345aa8e17
Java-Sith <prieto.luisdaniel@gmail.com>
2018-09-07 23:41:36 -0600

    Builder

---
+++ Builders/BuilderR/target/debug/BuilderR.d
@@ -0,0 +1,1 @@
+/home/zuckerberg/CLionProjects/BuilderR/target/debug/BuilderR: /home/zuckerberg/CLionProjects/BuilderR/src/main.rs
---
+++ Builders/BuilderR/target/debug/deps/BuilderR-871562c6f6fb6f8b.d
@@ -0,0 +1,5 @@
+/home/zuckerberg/CLionProjects/BuilderR/target/debug/deps/BuilderR-871562c6f6fb6f8b: src/main.rs
+
+/home/zuckerberg/CLionProjects/BuilderR/target/debug/deps/BuilderR-871562c6f6fb6f8b.d: src/main.rs
+
+src/main.rs:
